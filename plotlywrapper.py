9eb1dcb4247d0df539c4bdd849f3155b1becb32c
Jacques Kvam <jacques@verdigris.co>
2016-02-28 19:10:01 -0800

    add 3d line

--- plotlywrapper.py
+++ plotlywrapper.py
@@ -2,6 +2,7 @@
 
 from tempfile import NamedTemporaryFile
 
+# pylint: disable=redefined-builtin
 from builtins import zip
 
 import plotly.offline as py
@@ -143,6 +144,29 @@ def line(x=None, y=None, label=None, color=None, width=None, dash=None, opacity=
     else:
         data = [go.Scatter(x=x, y=y, name=label, line=lineattr, mode=mode,
                            fill=fill, opacity=opacity)]
+    return _Chart(data=data)
+
+
+def line3d(x, y, z, label=None, color=None, width=None, dash=None, opacity=None,
+           mode='lines', **kargs):
+    lineattr = {}
+    x = np.atleast_1d(x)
+    y = np.atleast_1d(y)
+    z = np.atleast_1d(z)
+    assert x.shape == y.shape
+    assert y.shape == z.shape
+    if y.ndim == 2:
+        if not hasattr(label, '__iter__'):
+            if label is None:
+                label = _labels()
+            else:
+                label = _labels(label)
+        data = [go.Scatter3d(x=xx, y=yy, z=zz, name=ll, line=lineattr, mode=mode,
+                             opacity=opacity)
+                for ll, xx, yy, zz in zip(label, x.T, y.T, z.T)]
+    else:
+        data = [go.Scatter3d(x=x, y=y, z=z, name=label, line=lineattr, mode=mode,
+                             opacity=opacity)]
     return _Chart(data=data)
 
 
